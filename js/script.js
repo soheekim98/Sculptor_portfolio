3dfa17b134f927b7bb9ea81c441c328eb6a5253d
soheekim98 <a01052989637@gmail.com>
2023-03-29 16:37:12 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+    // var swiper = new Swiper(".mySwiper", {
+    //     direction: "vertical",
+    //     loop: true,
+    //     speed:2500,
+    //     // touchRatio: 0,
+    //     resistance : false,
+    //     allowTouchMove : false,
+    //     autoplay: {
+    //         delay: 2500,
+    //         disableOnInteraction: false,
+    //     },
+
+    // });
+    media();
+    function media(){
+        let windowWidth = $(window).width();
+        if(windowWidth >= 1800){
+            var swiper = new Swiper(".mySwiper", {
+                direction: "vertical",
+                loop: true,
+                speed:2500,
+                // touchRatio: 0,
+                resistance : false,
+                allowTouchMove : false,
+                autoplay: {
+                    delay: 2500,
+                    disableOnInteraction: false,
+                },
+            });
+        }else{
+            
+        }
+    };
+
+    $('.btn li').click(function(){
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
+
+        const result = $(this).attr('data-alt');
+
+        $('.tabs > div').removeClass('active');
+        $(`#${result}`).addClass('active');
+
+    });
+
+    $('.nav-wrap').click(function(){
+        $(this).toggleClass('active');
+    });
+    $('.section-wrap').click(function(){
+        $('.nav-wrap').removeClass('active');
+    });
+
+
+});//end
